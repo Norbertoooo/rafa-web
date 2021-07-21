ac628d677e8d531fb0bdaca8526d2584e4200bc0
Vítor Norberto <vithor.norberto@gmail.com>
2021-07-21 09:23:35 -0300

    Adiciona spinner ao cadastrar terapeuta

--- src/app/components/cadastrar-terapeuta/cadastrar-terapeuta.component.ts
+++ src/app/components/cadastrar-terapeuta/cadastrar-terapeuta.component.ts
@@ -6,6 +6,7 @@ import {Router} from '@angular/router';
 import {AlertaService} from '../../shared/services/alerta.service';
 import {HttpErrorResponse, HttpResponse} from '@angular/common/http';
 import {EnderecoUtils} from '../../shared/utils/enderecoUtils';
+import {NgxSpinnerService} from 'ngx-spinner';
 
 @Component({
   selector: 'app-registrar',
@@ -18,7 +19,9 @@ export class CadastrarTerapeutaComponent implements OnInit {
   estados = [];
 
   constructor(private cadastrarTerapeutaService: CadastrarTerapeutaService,
-              private router: Router, private alertService: AlertaService) {
+              private router: Router,
+              private spinnerService: NgxSpinnerService,
+              private alertService: AlertaService) {
   }
 
   ngOnInit(): void {
@@ -26,16 +29,17 @@ export class CadastrarTerapeutaComponent implements OnInit {
   }
 
   cadastrar(): void {
-    console.log(this.terapeuta);
     this.formatarData(this.terapeuta.dataNascimento);
+    this.spinnerService.show();
     this.cadastrarTerapeutaService.cadastrarTerapeuta(this.terapeuta)
       .subscribe((resposta: HttpResponse<any>) => {
-        console.log(resposta);
         this.router.navigateByUrl('/').finally(() => {
           this.alertService.exibirSucesso('Cadastro efetuado com sucesso!');
+          this.spinnerService.hide();
         });
       }, (error: HttpErrorResponse) => {
         this.alertService.exibirErro(error.message);
+        this.spinnerService.hide();
       });
   }
 
